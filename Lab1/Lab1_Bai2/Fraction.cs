30085ade5461095495924f33f6ab3b39a3445341
HungMai <22520510@gm.uit.edu.vn>
2023-10-08 21:58:21 +0700

    lab1_bai2 add class Fraction

---
+++ Lab1/Lab1_Bai2/Fraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Bai2
+{
+    internal class Fraction
+    {
+        private int tu;
+        private int mau;
+
+        public int Tu { get => tu; set => tu = value; }
+        public int Mau { get => mau; set => mau = value; }
+
+        public Fraction() { }
+        public Fraction(int tu, int mau)
+        {
+            this.tu = tu;
+            this.mau = mau;
+        }
+    }
+}
---
+++ Lab1/Lab1_Bai2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lab1_Bai2;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
