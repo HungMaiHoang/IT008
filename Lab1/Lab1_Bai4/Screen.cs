d293e6de216a8e9a55d50f533ba9c3d05a19e5a9
HungMai <22520510@gm.uit.edu.vn>
2023-10-10 04:59:07 +0700

    add lab1_bai4

---
+++ Lab1/Lab1_Bai4/Screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Bai4
+{
+    internal class Screen
+    {
+        public void Display(EventHandling eventHandling)
+        {
+            Temperature temperature = new Temperature();
+            Console.WriteLine("Temperature: " + temperature.Degrees + " C");
+            Console.WriteLine("Press Space to change, other key to exit");
+            eventHandling.OnButtonPressed(this);
+        }        
+    }
+}
