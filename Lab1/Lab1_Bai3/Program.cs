987ed269e3da3feb6efca7e24306f9ccff08dd5e
NgoDuyHung2305 <22520511@gm.uit.edu.vn>
2023-10-15 20:03:27 +0700

    Lab1

---
+++ Lab1/Lab1_Bai3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Lab1_Bai3;
+using System;
+using System.Collections.Specialized;
+using System.Reflection.Emit;
+
+namespace Bai3
+{
+    class Program
+    {
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Nhap day");
+            string s = Console.ReadLine();
+            MaxMin a = new MaxMin();
+            a.Max(s);
+            a.Min(s);
+        }
+       
+    }
+}
---
+++ Lab1/Lab1_Bai6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace Bai6
+{
+   public interface IThinking
+    {
+        void thinking_behavior();
+    }
+   public interface IIntelligent
+    {
+        void intelligent_behavior();
+    }
+    interface IAbility : IThinking, IIntelligent
+    {
+
+    }
+    abstract class Mamal
+    {
+        public string characteristics;
+    }
+    class Whale : Mamal
+    {
+        Whale() 
+        {
+            characteristics = "whale";
+        }
+    }
+    class Human : Mamal, IAbility
+    {
+        Human()
+        {
+            characteristics = "human";
+        }
+        public void thinking_behavior()
+        { }
+        public void intelligent_behavior()
+        { }
+        static void Main(string[] args)
+        { }
+    }
+}
