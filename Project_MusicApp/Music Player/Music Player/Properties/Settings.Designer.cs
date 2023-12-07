cf93ff502a26046faf1a076e6f9ea9d7eee1f5b0
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2023-12-08 04:12:14 +0700

    add database

--- Project_MusicApp/Music Player/Music Player/Properties/Settings.Designer.cs
+++ Project_MusicApp/Music Player/Music Player/Properties/Settings.Designer.cs
@@ -22,5 +22,15 @@ namespace Music_Player.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MyDatabase.mdf;Integrated Security=True")]
+        public string MyDatabaseConnectionString {
+            get {
+                return ((string)(this["MyDatabaseConnectionString"]));
+            }
+        }
     }
 }
