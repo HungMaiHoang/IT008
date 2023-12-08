d419664c42b30f3199bad35002c5d07f3edcc5a3
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2023-12-09 01:29:58 +0700

    add

---
+++ Project_MusicApp/Music Player/Music Player/Models/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Player.Models
+{
+    internal class Song
+    {
+        private int songID;
+        private string title;
+        private string path;
+        private TimeSpan duration;
+
+        public int SongID { get => songID; set => songID = value; }
+        public string Title { get => title; set => title = value; }
+        public string Path { get => path; set => path = value; }
+        public TimeSpan Duration { get => duration; set => duration = value; }
+    }
+}
