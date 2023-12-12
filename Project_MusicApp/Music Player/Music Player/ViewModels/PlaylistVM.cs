afc1d74a36ff7d4463556d2bb0cc918a7038a1c5
DuyHung <22520511@gm.uit.edu.vn>
2023-12-12 23:53:03 +0700

    change Model

--- Project_MusicApp/Music Player/Music Player/ViewModels/PlaylistVM.cs
+++ Project_MusicApp/Music Player/Music Player/ViewModels/PlaylistVM.cs
@@ -14,27 +14,56 @@ namespace Music_Player.ViewModels
     public class PlaylistVM : ViewModelBase
     {
 
-        private static PlaylistVM instance =new PlaylistVM();
-        public static PlaylistVM Instance { get => instance;
-        }
+        //private static PlaylistVM instance;
+        //public static PlaylistVM Instance 
+        //{
+        //    get
+        //    {
+        //        if (instance == null)
+        //            instance = new PlaylistVM();
+        //        return instance;
+        //    }
+        //}
 
         private Playlist _playlist = new Models.Playlist();
-        public Playlist Playlist { get => _playlist; set { _playlist = value; OnPropertyChanged(nameof(Playlist)); } }
+        public Playlist Playlist { get => _playlist; set { _playlist = value; OnPropertyChanged(nameof(_playlist)); } }
+
+        private int _id;
+        private string _name;
+        public int Id { get => _id; set { _id = value; OnPropertyChanged(nameof(_id)); } }
+        public string Name { get => _name; set { _name = value; OnPropertyChanged(nameof(_name)); } }
+
         public ICommand DebugLog { get; set; }
 
-        public PlaylistVM(Playlist playlist) {
-            Playlist = playlist;
-            DebugLog = new RelayCommand(debugLog);
-        }
+        //public PlaylistVM(Playlist playlist) {
+        //    Playlist = playlist;
+        //    DebugLog = new RelayCommand(debugLog);
+        //}
         public PlaylistVM()
         {
-            instance = this;
+            Id = 0;
+            Name = "Unknow";
             DebugLog = new RelayCommand(debugLog);
         }
 
         private void debugLog(object obj)
         {
+            Name = "12345";
             MessageBox.Show(Playlist.Name);
+        }
+
+        public void ChangePlaylist(Models.Playlist curPlaylist)
+        {
+            if (curPlaylist == null)
+            {
+
+            }
+            else
+            {
+                Playlist = curPlaylist;
+                Id = curPlaylist.PlaylistID;
+                Name = curPlaylist.Name;
+            }
         }
     }
 }
